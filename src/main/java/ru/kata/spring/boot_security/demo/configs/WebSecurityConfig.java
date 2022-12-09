d148acadccb8a66fc999cd0ad7b884eee4f52988
Maksataiii <erkbvna@gmail.com>
2022-12-09 17:34:56 +0600

    отключила секьюрность для тестирования

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -22,10 +22,10 @@ protected void configure(HttpSecurity httpSecurity) throws Exception {
             .disable()
             .authorizeRequests()
             //Доступ только для не зарегистрированных пользователей
-            .antMatchers("/", "/register").permitAll()
+            .antMatchers("/", "/register","/admin/**","/user").permitAll()
             //Доступ только для пользователей с ролью Администратор
-            .antMatchers("/admin/**").hasRole("ADMIN")
-            .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+//            .antMatchers("/admin/**").hasRole("ADMIN")
+//            .antMatchers("/user").hasAnyRole("USER", "ADMIN")
 
             //Все остальные страницы требуют аутентификации
             .anyRequest().authenticated()
