0910b6ed611ca61a67cca1cb6abbd06d080365cf
Maksataiii <erkbvna@gmail.com>
2022-12-19 16:11:42 +0600

    исправила мелкие баги

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -33,7 +33,7 @@ public class User implements UserDetails {
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    @JsonIgnore
+
     private Collection<Role> roles;
 
     @Override
